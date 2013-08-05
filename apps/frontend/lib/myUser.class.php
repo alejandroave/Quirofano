a048b07f7ce042dcaa5956367e66eb5fe88f4da6
antsanch <antsanchez@gmail.com>
2013-08-05 17:51:21 -0500

    Agregados los estilos basicos del sigahu

--- apps/frontend/lib/myUser.class.php
+++ apps/frontend/lib/myUser.class.php
@@ -3,4 +3,41 @@
 //class myUser extends sfBasicSecurityUser
 class myUser extends sfGuardSecurityUser
 {
+ /* hasBasicProfile
+  * verifica que el usuario tenga completo un perfil basico
+  * @autor: Antonio Sanchez Uresti
+  * @date:  2013-08-05
+  */
+  public function hasBasicProfile()
+  {
+
+    return true;
+  }
+
+ /* getFullName
+  * Retorna el nombre completo del usuario
+  * @autor: Antonio Sanchez Uresti
+  * @date:  2013-08-05
+  */
+  public function getFullName()
+  {
+    return 'Usuario Ejemplo';
+  }
+
+  public function getServicioName()
+  {
+    return 'Ing. Biomedica';
+  }
+
+  public function getDepartamentoName()
+  {
+    return 'Ing. Biomedica';
+  }
+
+  public function getArea()
+  {
+    return 'Ing. Biomedica';
+  }
+
+
 }
